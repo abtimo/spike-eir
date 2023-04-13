9afd237232eb6727d360c09f004916f09148e550
Abtimo <sales@abtimo.com>
2023-04-13 19:10:53 +0200

    alpha version of the converter

--- mdgen/MarketingExpenses2ExpReport.md
+++ mdgen/MarketingExpenses2ExpReport.md
@@ -1,2 +1,18 @@
-| Source field   | Target field   | Transformation   |
-|----------------|----------------|------------------|
+## Table of Contents  
+  * [ Mapping - MarketingExpenses mapping](#-mapping---marketingexpenses-mapping)
+  
+#  Mapping - MarketingExpenses mapping  
+  
+| ExpReport | MarketingExpenses | mapping |  
+|:---:|:---:|:---:|  
+| /expense-report/Person | /marketing-expenses/Person | //defaultmap////// |  
+| /expense-report/Person/First | /marketing-expenses/Person/FullName | //defaultmap///concat////defaultmap////// |  
+| /expense-report/Person/Last | /marketing-expenses/Person/FullName | //defaultmap///concat////defaultmap////// |  
+| /expense-report/Person/Title | /marketing-expenses/Person/Title | //defaultmap////// |  
+| /expense-report/Person/Phone | /marketing-expenses/Person/Phone | //defaultmap////// |  
+| /expense-report/Person/Email | /marketing-expenses/Person/Email | //defaultmap////// |  
+| /expense-report/expense-item | /marketing-expenses/expense-item | //defaultmap////// |  
+| /expense-report/expense-item/type | /marketing-expenses/expense-item/type | //defaultmap////// |  
+| /expense-report/expense-item/Date | /marketing-expenses/expense-item/Date | //defaultmap////// |  
+| /expense-report/expense-item/expense | /marketing-expenses/expense-item/expense | //defaultmap////// |  
+  
--- mdgen/mfd2md.py
+++ mdgen/mfd2md.py
@@ -1,102 +1,191 @@
-import pandas as pd
-import xml.etree.ElementTree as ET
-import xmlschema
+import argparse
+from lxml import etree
+from markdowngenerator import MarkdownGenerator
 
 
-def map_schemas(schema1_file, schema2_file, mapping_file):
-    """Create a mapping file (.mfd) between two XSD schemas"""
+def increment_idx():
+    global idx
+    global column1h
+    global column2h
+    global column3h
+    idx = idx + 1
 
-    # Load the schemas
-    schema1 = ET.parse(schema1_file).getroot()
-    schema2 = ET.parse(schema2_file).getroot()
-
-    # Create the mapping file
-    mapping = ET.Element("Mapping")
-
-    # Iterate over all elements in the first schema
-    for el1 in schema1.iter():
-        # Find all matching elements in the second schema
-        xpath = el1.getroottree().getpath(el1)
-        el2 = schema2.find(xpath)
-
-        # If a matching element is found...
-        if el2 is not None:
-            # Create a MappingField element and set its attributes
-            field = ET.SubElement(mapping, "MappingField")
-            field.set("SourceSchema", schema1_file)
-            field.set("TargetSchema", schema2_file)
-            field.set("SourceField", el1.tag)
-            field.set("TargetField", el2.tag)
-
-    # Save the mapping to file
-    ET.SubElement(mapping, "Comments", {"Value": "Mapping created by script"})
-    tree = ET.ElementTree(mapping)
-    tree.write(mapping_file)
-
-    print(f"Mapping file created at {mapping_file}")
+    if len(table) <= idx:
+        line = {}
+        line[column1h] = ""
+        line[column2h] = ""
+        line[column3h] = ""
+        table.append(line)
 
 
-def mapping_to_md(mapping_file, md_file):
-    """Convert a mapping .mfd file to a Markdown table"""
+def main(mapping, mdfile):
+    print("Prepare mapping")
 
-    # Open the mapping file (.mfd)
-    tree = ET.parse(mapping_file)
-    root = tree.getroot()
+    global theroot
+    theroot = etree.parse(mapping)
 
-    # Create headers for Markdown table
-    headers = ["Source field", "Target field", "Transformation"]
-
-    # Create empty dataframe
-    df = pd.DataFrame(columns=headers)
-
-    # Iterate over elements inside <MappingField> tag
-    for field in root.iter("MappingField"):
-        row = {}
-        row["Source field"] = field.find("SourceField").text
-        row["Target field"] = field.find("TargetField").text
-        row["Transformation"] = field.find("Transformation").text
-
-        df = df.append(row, ignore_index=True)
-
-    # Convert dataframe to Markdown table
-    md_table = df.to_markdown(index=False)
-
-    # Save Markdown table to file
-    with open(md_file, "w") as f:
-        f.write(md_table)
-
-    print(f"Markdown table created at {md_file}")
+    for node in theroot.xpath("/mapping/component/structure/children/component"):
+        print(node.items())
+        print(theroot.getpath(node))
+    for component14 in theroot.findall(".//*[@kind='14']"):
+        print(type(component14))
+        print(component14.items())
+        if component14.find(".//*[@inpkey]") is None:
+            print("inpkey not found")
+            global leftmost
+            leftmost = component14
+        else:
+            print("inpkeyfound")
+            global rightmost
+            rightmost = component14
 
 
-def validate_xml(schema_file, xml_file):
-    """Validate an XML file against a given XSD schema"""
+def find_key(thekey, idx):
+    print("find_key " + thekey + " begin")
+    print("idx: " + str(idx))
 
-    # Load the XSD schema
-    xs = xmlschema.XMLSchema(schema_file)
+    # search inpkey
+    print("going to search inpkey")
+    if theroot.find(".//*[@inpkey='" + thekey + "']") is None:
+        print("no inpkey")
+    else:
+        print("inpkey found")
+        node_with_inpkey = theroot.find(".//*[@inpkey='" + thekey + "']")
+        print(theroot.getpath(node_with_inpkey))
+        name = node_with_inpkey.get("name")
+        print(name)
+        tollar = get_names(node_with_inpkey, "")
+        table[idx][column2h] = tollar
+        return node_with_inpkey
 
-    # Load the XML file
-    try:
-        xml = ET.parse(xml_file)
-    except ET.ParseError as e:
-        print(f"Invalid XML: {e}")
-        return
+    # search vertexkey
+    print("going to search vertexkey")
+    if theroot.find(".//vertex[@vertexkey='" + thekey + "']") is None:
+        print("no vertexkey")
+    else:
+        print("vertexkey found")
+        vertex = theroot.find(".//*[@vertexkey='" + thekey + "']")
+        edge = vertex.xpath("./edges/edge")
+        edge_vertexkey = edge[0].get("vertexkey")
+        print(edge_vertexkey)
+        tollar = get_names(edge[0], "")
+        table[idx][column3h] = table[idx].get(column3h) + tollar
+        find_key(edge_vertexkey, idx)
 
-    # Check if the XML is valid according to the schema
-    try:
-        xs.validate(xml)
-        print("XML is valid")
-    except xmlschema.exceptions.XMLSchemaValidationError as e:
-        print(f"Invalid XML: {e}")
+    # search source key
+    print("going to search source key")
+    if theroot.find(".//sources/datapoint[@key='" + thekey + "']") is None:
+        print("no source key")
+    else:
+        print("source key found")
+        source = theroot.find(".//sources/datapoint[@key='" + thekey + "']")
+        target = source.xpath(
+            "//sources/datapoint[@key='" + thekey + "']/../../targets/datapoint"
+        )
+        targetkey = target[0].get("key")
+        print(targetkey)
+        tollar = get_names(target[0], "")
+        table[idx][column3h] = tollar
+        find_key(targetkey, idx)
+
+    return None
 
 
-# Example usage
-#map_schemas("path/to/schema1.xsd", "path/to/schema2.xsd", "path/to/mapping.mfd")
-#mapping_to_md("path/to/mapping.mfd", "path/to/mapping_table.md")
-#validate_xml("path/to/schema.xsd", "path/to/data.xml")
+def follow_link(node):
+    print("follow_link begin")
 
-#from command line
-#$ python mfd2md.py
-#Markdown table created at ./MarketingExpenses2ExpReport.md
-#$
+    for child in node.getchildren():
+        global idx
+        increment_idx()
+        print(type(child))
+        print(child.items())
+        print(child.get("outkey"))
+        print(theroot.getpath(child))
+        tolarr = get_names(child, "")
+        table[idx][column1h] = tolarr
+        find_key(child.get("outkey"), idx)
+        follow_link(child)
+    return
 
-mapping_to_md("./MarketingExpenses2ExpReport.mfd", "./MarketingExpenses2ExpReport.md")
+
+def get_names(node, path):
+    if node.get("name") is not None:
+        node_name = node.get("name")
+    else:
+        node_name = ""
+    if node_name == "document":
+        return path
+    to_return = "/" + node_name + path
+    if node.getparent() is not None:
+        to_return = get_names(node.getparent(), to_return)
+    return to_return
+
+
+def produce_md(mdfile, table):
+    with MarkdownGenerator(
+        # By setting enable_write as False, content of the file is written
+        # into buffer at first, instead of writing directly into the file
+        # This enables for example the generation of table of contents
+        #filename="example.md", enable_write=False
+        filename=mdfile, enable_write=False
+    ) as doc:
+        doc.addHeader(1, " Mapping - " + column2h + " " + column3h )
+        #doc.writeTextLine(f'{doc.addBoldedText("This is just a test.")}')
+        #doc.addHeader(2, "Second level header.")
+        # table = [
+        #     {"Column1": "col1row1 data", "Column2": "col2row1 data"},
+        #     {"Column1": "col1row2 data", "Column2": "col2row2 data"},
+        # ]
+
+        doc.addTable(dictionary_list=table)
+        #doc.writeTextLine("Ending the document....")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Convert .mfd file to Markdown table or validate XML against XSD schema"
+    )
+    parser.add_argument("--mapping", help="Path to the mapping .mfd file")
+    parser.add_argument("--mdfile", help="Path to the output Markdown file")
+    args = parser.parse_args()
+
+    print(args)
+
+if args.mapping and args.mdfile:
+    # Convert a mapping file to a Markdown table
+    main(args.mapping, args.mdfile)
+
+    print("leftmost")
+    print(leftmost.items())
+    print("rightmost")
+    print(rightmost.items())
+
+    global column1h
+    column1h = leftmost.get("name")
+    global column2h
+    column2h = rightmost.get("name")
+    global column3h
+    column3h = "mapping"
+
+    table = []
+
+    global idx
+    idx = -1
+    for document_entry in leftmost.xpath("./data/root/entry/entry/entry/entry"):
+        document_entry_name = document_entry.get("name")
+        tolarr = get_names(document_entry, "")
+        increment_idx()
+        table[idx][column1h] = tolarr
+        find_key(document_entry.get("outkey"), idx)
+        follow_link(document_entry)
+    print("table:")
+    for line in table:
+        print(line)
+
+    produce_md(args.mdfile, table)
+
+else:
+    print(
+        "Error: specify either --mapping and --mdfile for mapping-to-Markdown conversion"
+        "Sample: mfd2md --mapping ./MarketingExpenses2ExpReport.mfd --mdfile ./MarketingExpenses2ExpReport.md"
+    )
